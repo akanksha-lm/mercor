002d8221760d8239f35f0271d5e914eac4ca5310
akanksha-lm <akanksha.sahu@leasingmonk.com>
2024-08-07 02:04:58 +0530

    chore: adding more api keys

--- src/contants/constants.js
+++ src/contants/constants.js
@@ -1,6 +1,7 @@
 export const DEFAULT_LOCATION = "California, United States";
 
 export const apiKeys = [
-  "mC3fORUFX8WFpE5wnaZ35qZUw4PXMWdzzzRmZCDO8lQ",
+  "W8drEwYBapY3Aow3vDLsCpGd1nlLJmdcx2Nft32RoGA",
   "ETV1y4FWm82KV9a6LSruGC1owvNgUNK2Nnu9BM-Rcz0",
+  "mC3fORUFX8WFpE5wnaZ35qZUw4PXMWdzzzRmZCDO8lQ",
 ];
